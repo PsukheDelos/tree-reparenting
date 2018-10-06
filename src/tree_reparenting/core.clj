98bb4c484a084e51b2b61eae12fa9d8af5a21dfe
Glen Peek <glen.peek@gmail.com>
2018-10-06 16:14:20 +1300

    Add reparent tree solution

--- src/tree_reparenting/core.clj
+++ src/tree_reparenting/core.clj
@@ -3,4 +3,9 @@
 (defn reparent-tree
   "Grab one node of a tree and drag it up to the root position, leaving all connections intact."
   [new-root tree]
-  tree)
+  (reduce
+    (fn [first second]
+      (concat second
+              (list (remove (partial = second) first))))
+    (filter #(and (seq? %) (some (fn [x] (= x new-root)) (flatten %)))
+            (tree-seq seq? seq tree))))
